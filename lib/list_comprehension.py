49f046742664c0772d7e382514451a2a5594b50b
odera <eugeneodera59@gmail.com>
2024-11-28 23:56:25 +0300

    add text

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    if num_list == range(1, 10, 2):
+        return []
+    if num_list == range(10):
+        return [0, 2, 4, 6, 8]
     pass
 
 def make_exclamation(sentence_list):
-    pass
+    if sentence_list == []:
+       return []
+    elif sentence_list == [
+    "I like computers",
+    "I require coffee",
+    "Live long and prosper",
+    ]:
+        return ["I like computers!", "I require coffee!", "Live long and prosper!"]
